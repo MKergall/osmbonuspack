d02be87a513b638f20db6f0fbe174d5547987b65
Hannes Achleitner <hannes.software@gmx.at>
2020-07-01 06:29:23 +0200

    Android X

--- OSMBonusPack/src/main/java/org/osmdroid/bonuspack/routing/MapQuestRoadManager.java
+++ OSMBonusPack/src/main/java/org/osmdroid/bonuspack/routing/MapQuestRoadManager.java
@@ -5,7 +5,6 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.osmdroid.bonuspack.utils.BonusPackHelper;
-import org.osmdroid.bonuspack.utils.PolylineEncoder;
 import org.osmdroid.util.BoundingBox;
 import org.osmdroid.util.GeoPoint;
 import java.util.ArrayList;
